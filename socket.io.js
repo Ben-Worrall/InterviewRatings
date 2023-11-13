f5f0eac4160614001e106978b0f458d9da5a9a80
Ben <benworrallpc@gmail.com>
2023-11-13 11:18:10 +0200

    homescreen

---
+++ socket.io.js
@@ -0,0 +1,11 @@
+var JoinRoom = document.getElementById('JoinRoom')
+var CreateRoom = document.getElementById('CreateRoom')
+
+
+JoinRoom.addEventListener('click', function(){
+    console.log('JoinRoom')
+})
+
+CreateRoom.addEventListener('click', function(){
+    console.log('CreateRoom')
+})
