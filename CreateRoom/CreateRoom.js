7be7c2cd3c74552bc92d7470b6924b26224fa9b2
Ben <benworrallpc@gmail.com>
2023-11-14 11:12:36 +0200

    (random password) try mobile sideways

--- CreateRoom/CreateRoom.js
+++ CreateRoom/CreateRoom.js
@@ -1,3 +1,22 @@
+
+//craete random password
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    document.getElementById('RoomPasswordText').innerHTML = result
+    
+}
+
+generateString(12);
+
+
+
 //copy the room passsword
 var RoomPassword = document.getElementById('RoomPasswordText').innerHTML
 
